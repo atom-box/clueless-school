0f12b04645597483b4797107bfe2b11cb62bb291
atom-box <atom_box@hotmail.com>
2021-10-03 10:47:18 -0500

    Make placeholder HTML with Choka CSS.

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+
+    <link rel="stylesheet" href="node_modules/chota/dist/chota.css">
+</head>
+
+<body>
+
+
+
+    <div class="container">
+
+
+    <div class="hero is-full-screen">
+    <section id="features">
+      <h2>cLueLess.school</h2>
+      <ul>
+        <li>⚡️&nbsp; CSS by Chota</li>
+        <li>📐&nbsp; Loaded <a href="#grid">grid</a></li>
+        <li>🌈&nbsp; With real fruit!</li>
+        <li>🤡&nbsp; Supports icons out-of-the-box</li>
+      </ul>
+    </section>
+    </div>
+
+
+
+
+   </div>
+
+
+</body>
+
+</html>
